2a986b5c489ae85f51b3465e9625234ee5f83810
Mücahit Aydın <mucahitaydin@isyazilim.com.tr>
2023-10-13 22:02:42 +0300

    first commit

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/providers/user_provider.dart';
+import 'package:instagram_clone/utils/global_variables.dart';
+import 'package:provider/provider.dart';
+
+class ReponsiveLayout extends StatefulWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ReponsiveLayout({
+    Key? key,
+    required this.mobileScreenLayout,
+    required this.webScreenLayout,
+  }) : super(key: key);
+
+  @override
+  State<ReponsiveLayout> createState() => _ReponsiveLayoutState();
+}
+
+class _ReponsiveLayoutState extends State<ReponsiveLayout> {
+  @override
+  void initState() {
+    super.initState();
+    addData();
+  }
+
+  addData()  async {
+    UserProvider _userProvider = Provider.of(context, listen: false);
+    await _userProvider.refreshUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > webScreenSize) {
+          return widget.webScreenLayout;
+        }
+        return widget.mobileScreenLayout;
+      },
+    );
+  }
+}
